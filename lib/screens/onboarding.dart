a76327b06b2150bd368c7cf2a87c22af36faf4ba
Michael Timi <michtimiael@gmail.com>
2022-07-11 17:15:46 +0100

    onboarding screen done

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -25,102 +25,102 @@ class _OnBoardingState extends State<OnBoarding> {
             Expanded(
                 flex: 4,
                 child: Container(
-                  padding: const EdgeInsets.all(32),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          offset: const Offset(0, -3),
-                          blurRadius: 5,
-                        )
-                      ],
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(28),
-                          topRight: Radius.circular(28))),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'Read the article you\nwant instantly',
-                        style: GoogleFonts.poppins(
-                            textStyle: const TextStyle(
-                                fontWeight: FontWeight.w700, fontSize: 24)),
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        'You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones.',
-                        style: GoogleFonts.poppins(
-                            textStyle: const TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 14,
-                                height: 2,
-                                color: Color(0xff2D4379))),
-                      ),
-                      const SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                height: 14,
-                                width: 14,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color(0xffDEE7FF),
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              Container(
-                                height: 14,
-                                width: 14,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color(0xffDEE7FF),
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              Container(
-                                height: 14,
-                                width: 14,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color(0xffDEE7FF),
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              Container(
-                                height: 14,
-                                width: 30,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(26),
-                                  shape: BoxShape.rectangle,
-                                  color: const Color(0xff376AED),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                              height: 60,
-                              width: 90,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                shape: BoxShape.rectangle,
-                                color: const Color(0xff376AED),
-                              ),
-                              child: const Center(
-                                  child: Icon(
-                                Icons.arrow_forward,
-                                color: Colors.white,
-                              )))
-                        ],
-                      )
-                    ],
+              padding: const EdgeInsets.all(32),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: const Offset(0, -3),
+                      blurRadius: 5,
+                    )
+                  ],
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(28),
+                      topRight: Radius.circular(28))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'Read the article you\nwant instantly',
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 24)),
                   ),
-                )),
+                  const SizedBox(height: 20),
+                  Text(
+                    'You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones.',
+                    style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                            height: 2,
+                            color: Color(0xff2D4379))),
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 14,
+                            width: 14,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xffDEE7FF),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            height: 14,
+                            width: 14,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xffDEE7FF),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            height: 14,
+                            width: 14,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xffDEE7FF),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            height: 14,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(26),
+                              shape: BoxShape.rectangle,
+                              color: const Color(0xff376AED),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                          height: 60,
+                          width: 90,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            shape: BoxShape.rectangle,
+                            color: const Color(0xff376AED),
+                          ),
+                          child: const Center(
+                              child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                          )))
+                    ],
+                  )
+                ],
+              ),
+            )),
           ],
         ),
       ),
